c4262b6bfdf05a71d0e87453a79edf4387ed4edd
Dzamalt <77825337+Dzamalt@users.noreply.github.com>
2023-08-06 10:14:58 +0700

    Initial commit

---
+++ src/main/java/net/dzam/originsoath/OriginsOathClient.java
@@ -0,0 +1,10 @@
+package net.dzam.originsoath;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class OriginsOathClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
---
+++ src/main/java/net/dzam/originsoath/OriginsOathDataGenerator.java
@@ -0,0 +1,11 @@
+package net.dzam.originsoath;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class OriginsOathDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
